da08d2faae8b0353c4cfc5fa83459757723c6722
VicSDN <victormenendez2011@gmail.com>
2025-06-23 02:28:25 +0200

    Changes .img

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,36 @@ module.exports = defineConfig({
     orientation: "portrait-primary",
     background_color: "#0d0d0d",
     theme_color: "#8b5cf6",
-    workboxPluginMode: "InjectManifest",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    // Use default GenerateSW mode instead of InjectManifest
+    workboxPluginMode: "GenerateSW",
     workboxOptions: {
-      swSrc: "src/sw.js",
-      swDest: "service-worker.js",
+      // Cache strategies
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\//,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "google-fonts",
+            expiration: {
+              maxEntries: 30,
+              maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+            },
+          },
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+            },
+          },
+        },
+      ],
     },
     manifestOptions: {
       icons: [
@@ -24,8 +50,44 @@ module.exports = defineConfig({
           purpose: "any",
         },
         {
+          src: "/img/icons/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "/img/icons/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "/img/icons/icon-128x128.png",
+          sizes: "128x128",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "/img/icons/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "/img/icons/icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
           src: "/img/icons/icon-192x192.png",
           sizes: "192x192",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "/img/icons/icon-384x384.png",
+          sizes: "384x384",
           type: "image/png",
           purpose: "any maskable",
         },
